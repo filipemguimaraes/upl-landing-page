117f5f3f0e1c509200ad0d1b3d19b79931f0d276
gui <filipe.guimaraes@datahouse.ch>
2024-11-14 18:10:11 +0000

    improvements Testimonial

--- src/components/CallToAction/CallToAction.js
+++ src/components/CallToAction/CallToAction.js
@@ -1,74 +1,48 @@
 import React from "react";
-import {
-  Box,
-  Card,
-  CardContent,
-  Typography,
-  Button,
-  Divider,
-} from "@mui/material";
+import { Card, CardContent, Typography, Button, Divider } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import "./CallToAction.css";
+
+// Array containing the actions
+const actions = [
+  {
+    title: "Adere à nossa Comunidade WhatsApp",
+    text: "Participa diretamente nas discussões que estão a moldar o futuro do partido. Unidos, trocamos ideias e definimos o caminho para um Portugal mais liberal. Entrar no grupo WhatsApp.",
+    buttonText: "ENTRAR NO GRUPO WHATSAPP",
+  },
+  {
+    title: "Contribui com um Donativo para a Campanha",
+    text: "O teu apoio financeiro é essencial para mantermos a campanha viva e levarmos as nossas ideias a cada canto de Portugal. Cada contribuição conta!",
+    buttonText: "FAZER UM DONATIVO",
+  },
+  {
+    title: "No Dia das Eleições, Vota Lista U",
+    text: "O culminar do nosso trabalho conjunto. No dia das eleições, faz a diferença: vota na Lista U para que possamos restaurar os valores liberais e construir um futuro mais justo e livre.",
+    buttonText: "INSCREVER-ME NA CONVENÇÃO",
+  },
+];
 
 export const CallToAction = () => {
   return (
     <Grid container spacing={4} className="call-to-action">
-      <Grid item xs={12} md={6}>
-        <Card className="cta-card">
-          <CardContent>
-            <Typography variant="h5" className="cta-title">
-              Adere à nossa Comunidade WhatsApp
-            </Typography>
-            <Divider className="cta-divider" />
-            <Typography variant="body1" className="cta-text">
-              Participa diretamente nas discussões que estão a moldar o futuro
-              do partido. Unidos, trocamos ideias e definimos o caminho para um
-              Portugal mais liberal. Entrar no grupo WhatsApp.
-            </Typography>
-            <Button variant="contained" color="error" className="cta-button">
-              ENTRAR NO GRUPO WHATSAPP
-            </Button>
-          </CardContent>
-        </Card>
-      </Grid>
-
-      <Grid item xs={12} md={6}>
-        <Card className="cta-card">
-          <CardContent>
-            <Typography variant="h5" className="cta-title">
-              Contribui com um Donativo para a Campanha
-            </Typography>
-            <Divider className="cta-divider" />
-            <Typography variant="body1" className="cta-text">
-              O teu apoio financeiro é essencial para mantermos a campanha viva
-              e levarmos as nossas ideias a cada canto de Portugal. Cada
-              contribuição conta!
-            </Typography>
-            <Button variant="contained" color="error" className="cta-button">
-              FAZER UM DONATIVO
-            </Button>
-          </CardContent>
-        </Card>
-      </Grid>
-
-      <Grid item xs={12} md={6}>
-        <Card className="cta-card">
-          <CardContent>
-            <Typography variant="h5" className="cta-title">
-              No Dia das Eleições, Vota Lista U
-            </Typography>
-            <Divider className="cta-divider" />
-            <Typography variant="body1" className="cta-text">
-              O culminar do nosso trabalho conjunto. No dia das eleições, faz a
-              diferença: vota na Lista U para que possamos restaurar os valores
-              liberais e construir um futuro mais justo e livre.
-            </Typography>
-            <Button variant="contained" color="error" className="cta-button">
-              INSCREVER-ME NA CONVENÇÃO
-            </Button>
-          </CardContent>
-        </Card>
-      </Grid>
+      {actions.map((action, index) => (
+        <Grid item xs={12} md={6} key={index}>
+          <Card className="cta-card">
+            <CardContent>
+              <Typography variant="h5" className="cta-title">
+                {action.title}
+              </Typography>
+              <Divider className="cta-divider" />
+              <Typography variant="body1" className="cta-text">
+                {action.text}
+              </Typography>
+              <Button variant="contained" color="error" className="cta-button">
+                {action.buttonText}
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
     </Grid>
   );
 };
--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,71 +1,85 @@
 import React from "react";
-import { Box, Typography, Paper, Button } from "@mui/material";
+import { Typography, Paper, Button } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import doubleQuotesIcon from "../../assets/svg/double-quotes.svg";
 import "./Testimonials.css";
 
 export const Testimonials = () => {
   return (
-    <Box className="testimonials-section">
-      <Typography variant="h4" className="testimonials-title">
-        Testemunhos de membros
-      </Typography>
-
-      <Box className="testimonials">
-        <Paper className="testimonial" elevation={3}>
-          <Typography variant="body1">
-            "Tive oportunidade de contactar, de forma muito direta, com
-            conivências entre núcleos e órgãos centrais, que me pareceram no
-            mínimo incompatíveis com um partido que se diz liberal. Para além
-            disso estou desiludido com a oportunidade falhada que a IL teve
-            nestas últimas eleições, fruto de uma estratégia divisionista da CE
-            e que se revelou desastrosa." - Tiago
+    <div className="testimonials-section">
+      <Grid container justifyContent="center" spacing={4}>
+        <Grid
+          item
+          xs={12}
+          md={3}
+          className="left-section"
+          display="flex"
+          flexDirection="column"
+        >
+          <Typography variant="h4" className="testimonials-title">
+            Testemunhos de membros
           </Typography>
-        </Paper>
+          <img
+            src={doubleQuotesIcon}
+            alt="Quotes"
+            style={{ width: "100%", maxHeight: "200px" }}
+          />
+        </Grid>
 
-        <Paper className="testimonial" elevation={3}>
-          <Typography variant="body1">
-            "Decidi juntar-me ao partido uma vez que me identifico totalmente
-            com o liberalismo em toda a linha, por este defendido. Acredito que
-            este liberalismo tem espaço para crescer em Portugal. Porém, para
-            que este crescimento seja possível, o partido tem de aplicar tais
-            conceitos internamente, promovendo maior coerência." - Mário
-          </Typography>
-        </Paper>
-
-        <Paper className="testimonial" elevation={3}>
-          <Typography variant="body1">
-            "Sou um mero militante da IL que não ambiciona fazer carreira
-            política, mas que acredita e quer ajudar a fazer um Portugal melhor
-            para as futuras gerações. Faço parte do grupo de militantes que está
-            descontente com o rumo que o partido seguiu nos últimos meses, onde
-            na minha opinião ficou evidente a falta de estratégia política para
-            o seu eleitorado, mas infelizmente a estratégia/agenda pessoal
-            passaram a ser 'normal'." - Frederico
-          </Typography>
-        </Paper>
-
-        <Paper className="testimonial" elevation={3}>
-          <Typography variant="body1">
-            "Para espanto meu, encontrei a maioria das ideias certas, mas o
-            partido errado. Parece um contrassenso, mas o tempo, cerca de dois
-            anos, demonstrou-me que era mesmo assim: Um partido de ideias
-            genericamente liberais, mas com uma estrutura completamente
-            iliberal, centralista e com 'jeitos' ditatoriais, pior que o PCP que
-            eu combati toda a minha vida." - João
-          </Typography>
-        </Paper>
-
-        <Paper className="testimonial" elevation={3}>
-          <Typography variant="body1">
-            "Juntei-me à IL em 2021 porque o liberalismo faz todo o sentido e
-            faz falta a Portugal. Estamos juntos nesta missão, de construir um
-            Portugal melhor, mais liberal." - Filipe
-          </Typography>
-        </Paper>
-      </Box>
-
-      <Button variant="contained" className="cta-button">
-        JUNTA-TE A ESTES E OUTROS MEMBROS NA NOSSA COMUNIDADE NO WHATSAPP!
-      </Button>
-    </Box>
+        <Grid item xs={12} md={9} className="right-section">
+          {testimonials.map((testimonial, index) => (
+            <Paper
+              key={index}
+              className="testimonial"
+              elevation={3}
+              style={{ margin: "10px", padding: "20px" }}
+            >
+              <Typography variant="body1" style={{ whiteSpace: "pre-wrap" }}>
+                "{testimonial.quote}"
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                style={{ textAlign: "right", marginTop: "10px" }}
+              >
+                - {testimonial.author}
+              </Typography>
+            </Paper>
+          ))}
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" className="testimonials-button">
+          JUNTA-TE A ESTES E OUTROS MEMBROS NA NOSSA COMUNIDADE NO WHATSAPP!
+        </Button>
+      </Grid>
+    </div>
   );
 };
+
+const testimonials = [
+  {
+    author: "Tiago",
+    quote:
+      "Tive oportunidade de contactar, de forma muito direta, com conivências entre núcleos e órgãos centrais, que me pareceram no mínimo incompatíveis com um partido que se diz liberal. Para além disso estou desiludido com a oportunidade falhada que a IL teve nestas últimas eleições, fruto de uma estratégia divisionista da CE e que se revelou desastrosa.",
+  },
+  {
+    author: "Mário",
+    quote:
+      "Decidi juntar-me ao partido uma vez que me identifico totalmente com o liberalismo em toda a linha, por este defendido. Acredito que este liberalismo tem espaço para crescer em Portugal. Porém, para que este crescimento seja possível, o partido tem de aplicar tais conceitos internamente, promovendo maior coerência.",
+  },
+  {
+    author: "Frederico",
+    quote:
+      "Sou um mero militante da IL que não ambiciona fazer carreira política, mas que acredita e quer ajudar a fazer um Portugal melhor para as futuras gerações. Faço parte do grupo de militantes que está descontente com o rumo que o partido seguiu nos últimos meses, onde na minha opinião ficou evidente a falta de estratégia política para o seu eleitorado, mas infelizmente a estratégia/agenda pessoal passaram a ser 'normal'.",
+  },
+  {
+    author: "João",
+    quote:
+      "Para espanto meu, encontrei a maioria das ideias certas, mas o partido errado. Parece um contrassenso, mas o tempo, cerca de dois anos, demonstrou-me que era mesmo assim: Um partido de ideias genericamente liberais, mas com uma estrutura completamente iliberal, centralista e com 'jeitos' ditatoriais, pior que o PCP que eu combati toda a minha vida.",
+  },
+  {
+    author: "Filipe",
+    quote:
+      "Juntei-me à IL em 2021 porque o liberalismo faz todo o sentido e faz falta a Portugal. Estamos juntos nesta missão, de construir um Portugal melhor, mais liberal.",
+  },
+];
