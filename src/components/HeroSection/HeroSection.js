8d6401afeb2ac15b21de31e12b23ddfd973e6745
gui <filipe.guimaraes@datahouse.ch>
2024-12-08 21:59:02 +0000

    HeroSection responsive mobile

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -5,34 +5,30 @@ import "./HeroSection.css";
 
 export const HeroSection = () => {
   return (
-    <Box component="section" id="hero-section" className="hero-section">
-      <Grid container xs={12}>
-        <Grid item xs={12} md={6}>
-          <Box className="hero-content">
-            <Typography variant="h3" className="hero-title">
-              O Liberalismo Precisa de Ti
-            </Typography>
-            <Typography variant="h5" className="hero-subtitle">
-              Portugal Precisa de Nós
-            </Typography>
-            <Typography variant="body1" className="hero-text">
-              Para mudar Portugal, temos primeiro de mudar o Partido. Unidos,
-              podemos refundar o Partido e devolver-lhe os valores Liberais que
-              sempre defendemos. Com coragem e ambição, reformaremos a nossa
-              estrutura, restaurando a liberdade, transparência e mérito.
-            </Typography>
-            <Button variant="contained" className="manifesto-button">
-              CONHECE O NOSSO MANIFESTO
-            </Button>
-          </Box>
-        </Grid>
+    <Grid container xs={12} id="hero-section" className="hero-section">
+      <Grid item xs={12} className="hero-content">
+        <Typography variant="h3" className="hero-title">
+          O Liberalismo Precisa de Ti
+        </Typography>
+        <Typography variant="h5" className="hero-subtitle">
+          Portugal Precisa de Nós
+        </Typography>
+        <Typography variant="body1" className="hero-text">
+          Para mudar Portugal, temos primeiro de mudar o Partido. Unidos,
+          podemos refundar o Partido e devolver-lhe os valores Liberais que
+          sempre defendemos. Com coragem e ambição, reformaremos a nossa
+          estrutura, restaurando a liberdade, transparência e mérito.
+        </Typography>
+        <Button variant="contained" className="manifesto-button">
+          CONHECE O NOSSO MANIFESTO
+        </Button>
       </Grid>
-      <Box className="join-us">
+      <Grid item xs={12} className="join-us">
         <Button variant="contained" className="join-button">
           JUNTA-TE A NÓS!
         </Button>
-      </Box>
-      <Box className="pink-line" />
-    </Box>
+      </Grid>
+      <Grid item xs={12} className="pink-line" />
+    </Grid>
   );
 };
